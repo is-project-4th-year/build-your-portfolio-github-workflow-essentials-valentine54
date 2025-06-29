7b2e329f0e4447db9f49eef0ba373f54e171fca1
valentine54 <wanjiku.valentine@strathmore.edu>
2025-06-29 20:42:39 +0300

    build: Add deploy script to package.json
    This is to delpoy the app to GitHub Pages using the `gh-pages` package.
    
    Issue: #13

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// import ghPages from 'vite-plugin-gh-pages'
 
+const repoName = 'build-your-portfolio-github-workflow-essentials-valentine54'
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [
     react(),
-    tailwindcss()],
+    tailwindcss()
+  ],
+    
   
 })
