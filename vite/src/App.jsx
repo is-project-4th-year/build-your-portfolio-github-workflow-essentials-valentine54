e20ed50c22f008dbd7f6af0162c859230c89e834
valentine54 <wanjiku.valentine@strathmore.edu>
2025-06-29 17:07:50 +0300

    feat(experience): add experience section with 3 professional roles
    Introduced a new Experience component showing internship, volunteer, and academic background in a styled layout.
    
    Issue: is-project-4th-year/build-your-portfolio-github-workflow-essentials-valentine54#5

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import AboutMe from './components/AboutMe';
 import EducationSection from './components/EducationSection';
 import Projects from './components/Projects';
+import Experience from './components/Experience';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <AboutMe />
       <EducationSection />
       <Projects />
+      <Experience />
 
     </div>
   );
